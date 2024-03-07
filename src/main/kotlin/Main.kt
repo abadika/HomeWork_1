02e893f38e91b208eadefd1590fc02b8c50e7fb1
Renakos <renakos0129@gmail.com>
2024-03-07 18:04:58 +0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    var Myfrend: String = " Hi, how are you"
+    val NUM: Int = 10
+    var word: String = " Abadika"
+    var result: String = NUM.toString() + word
+    println("$Myfrend $NUM $word")
+    when{
+        NUM <0-> println("Вы сохранили отрицательное число")
+        NUM <0-> println("Вы сохранили положительное число")
+        else-> println("Вы соханили ноль")
+
+    }
+
+}
